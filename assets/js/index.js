7c297dbb453e8f224fdd2635ac7e8193eec95a7a
arezu1368 <zahra.kar_gar@yahoo.com>
2020-07-05 10:33:43 +0430

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,22 +15,25 @@ export class Index {
     searchBtnClick = () => {
         this.removeNodes();
         const key = document.getElementById("key").value;
-        const bookServices = new  BookServices();
-        const res = bookServices.search(key);
-        this.showListItems(res);
+        if(key !== undefined && key.length > 0) {
+            const bookServices = new  BookServices();
+            const res = bookServices.search(key);
+            this.showListItems(res);
+        }
     };
     showListItems = (res) => {
         res.then(function(data) {
             const items = data.items;
-            for (const item of items) {
-                let resultItem = new ResultItem();
-                resultItem = item;
-                let volumeInfo = new Volume();
-                volumeInfo = resultItem.volumeInfo;
-                const bookCard = new BookCard(volumeInfo);
-                bookCard.createCard();
+            if(items !== undefined){
+                for (const item of items) {
+                    let resultItem = new ResultItem();
+                    resultItem = item;
+                    let volumeInfo = new Volume();
+                    volumeInfo = resultItem.volumeInfo;
+                    const bookCard = new BookCard(volumeInfo);
+                    bookCard.createCard();
+                }
             }
-
         })
     }
 }
